cec74a6843bf74b52771b9766c60b029bf1be878
Adam Gamble <adamgamble@gmail.com>
2013-10-27 23:46:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iwod::Application.config.secret_key_base = 'e43da3b7b97669f40065d4930218adbbb3e61651dd36b2006d020525fc9293dba05a10111dd2d71a8a19455ee59d4953725ac4bdfa41c2bd17bfcc391236fe9f'
